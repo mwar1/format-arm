f73f841dddbf3ada0a7c1a9aa196e49411466a99
Max Waring <maxim.waring@student.manchester.ac.uk>
2023-10-24 22:40:41 +0100

    Allow the pattern we're aligning to be anywhere in the line of code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,52 @@
 import * as vscode from 'vscode';
 
+function getLastLetter(line: string, pattern: string){
+	var whitespace = 0;
+
+	var words = [];
+	var currentWord = "";
+	var foundFirst = false;
+	for (const letter of line) {
+		if (letter == " " || letter == "\t") {
+			if (foundFirst && currentWord.length > 0) {
+				words.push(currentWord);
+				currentWord = "";
+			}
+
+			whitespace++;
+		} else {
+			foundFirst = true;
+			currentWord += letter;
+		}
+	}
+	
+	var total = whitespace;
+	for (let i=0; i<words.length; i++) {
+		total += words[i].length;
+	}
+
+	return total;
+}
+
 async function alignPattern(editor: vscode.TextEditor, pattern: string) {
 	const regexp = new RegExp(".+(" + pattern + ")", "gmi");
 	let toReplace: number[][];
 	toReplace = [];
-
+	
 	var maxIndex = 0;
 	for (let i=0; i<editor.document.lineCount; i++) {
 		var thisLine = editor.document.lineAt(i).text;
-		if (thisLine.match(regexp)) {
-			var j = 0;
-			while (thisLine[j] != " " && thisLine[j] != "\t") {
-				j++;
-			}
-			toReplace.push([i, j]);
-			
-			if (j > maxIndex) maxIndex = j;
+		var regexMatch = thisLine.match(regexp);
+		if (regexMatch) {
+			let index = getLastLetter(regexMatch[0], pattern);
+			toReplace.push([i, index]);
+			if (index > maxIndex) maxIndex = index;
 		}
 	}
 
 	await editor.edit(editBuilder => {
 		for (let i=0; i<toReplace.length; i++) {
-			var start = new vscode.Position(toReplace[i][0], toReplace[i][1]+1);
+			var start = new vscode.Position(toReplace[i][0], toReplace[i][1]);
 
 			var diff = maxIndex - toReplace[i][1];
 			editBuilder.insert(start, " ".repeat(diff));
@@ -34,7 +59,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand("format-arm.format", async () => {
 		vscode.window.showInformationMessage("Formatting ARM code...");
 
-		let patterns = ["defw", "defb"];
+		let patterns = ["defw", "defb", ";"];
 
 		for (const p of patterns) {
 			let editor = vscode.window.activeTextEditor;
