437c604ad11e46fc518424160d2a41643c642198
Max Waring <maxim.waring@student.manchester.ac.uk>
2023-10-24 22:04:34 +0100

    Align DEFW and DEFB

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,47 @@
 import * as vscode from 'vscode';
 
+async function alignPattern(editor: vscode.TextEditor, pattern: string) {
+	const regexp = new RegExp(".+(" + pattern + ")", "gmi");
+	let toReplace: number[][];
+	toReplace = [];
+
+	var maxIndex = 0;
+	for (let i=0; i<editor.document.lineCount; i++) {
+		var thisLine = editor.document.lineAt(i).text;
+		if (thisLine.match(regexp)) {
+			var j = 0;
+			while (thisLine[j] != " " && thisLine[j] != "\t") {
+				j++;
+			}
+			toReplace.push([i, j]);
+			
+			if (j > maxIndex) maxIndex = j;
+		}
+	}
+
+	await editor.edit(editBuilder => {
+		for (let i=0; i<toReplace.length; i++) {
+			var start = new vscode.Position(toReplace[i][0], toReplace[i][1]+1);
+
+			var diff = maxIndex - toReplace[i][1];
+			editBuilder.insert(start, " ".repeat(diff));
+		}
+	})
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand("format-arm.format", () => {
+	let disposable = vscode.commands.registerCommand("format-arm.format", async () => {
 		vscode.window.showInformationMessage("Formatting ARM code...");
+
+		let patterns = ["defw", "defb"];
+
+		for (const p of patterns) {
+			let editor = vscode.window.activeTextEditor;
+			if (!editor) return;
+			
+			await alignPattern(editor, p);
+		};
 	});
 
 	context.subscriptions.push(disposable);
